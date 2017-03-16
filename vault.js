648366b629178daef83d4e5c6b0a485ffccd6388
Alexander Davis <akonidavis@gmail.com>
2017-03-15 14:59:41 -1000

    vault opened

--- vault.js
+++ vault.js
@@ -1,4 +1,23 @@
 'use strict';
-module.exports = function() {
-  
-};
+module.exports = (function() {
+	var vault = {};
+	var password = {};
+
+	function setValue(key,value){
+  		vault = value;
+  		password = key;
+  	}
+
+	function getValue(key){
+  		if (key === password){
+  			return vault;
+  		} else {
+  			return null;
+  		}
+	}
+	
+	return {
+		setValue,
+		getValue,
+	};
+});
